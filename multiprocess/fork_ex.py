c53281e78ebe1a7eea41ee0f8b2e2555a0be49ae
taekwan-hwang <xorhks0005@gmail.com>
2019-01-12 21:03:20 +0900

    Add 'multiprocess/fork_ex.py'

---
+++ multiprocess/fork_ex.py
@@ -0,0 +1,13 @@
+"""
+os.fork() example
+"""
+
+import os
+import logger
+
+if __name__ == '__main__':
+    pid = os.fork()
+    if pid == 0:
+        logger.info('this is child process. pid: %d, child_pid: %d', os.getpid(), pid)
+    else:
+        logger.info('this is parent process. pid: %d, child_pid: %d', pid, os.getpid())
