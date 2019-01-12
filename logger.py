c1e8e03d26287031882729a280498dcf8cd9ba8d
taekwan-hwang <xorhks0005@gmail.com>
2019-01-12 21:06:46 +0900

    Add 'logger.py'

---
+++ logger.py
@@ -0,0 +1,25 @@
+"""
+전역적으로 사용할 logger
+"""
+
+
+import logging
+
+_logger = logging.getLogger('logger')
+
+_logger.setLevel(logging.DEBUG)
+
+_stream_handler = logging.StreamHandler()
+_formatter = logging.Formatter('%(process)s %(processName)s %(thread)s %(threadName)s\
+%(filename)s line %(lineno)s, %(asctime)s %(levelname)s: %(message)s')
+_stream_handler.setFormatter(_formatter)
+
+_logger.addHandler(_stream_handler)
+
+info = _logger.info
+debug = _logger.debug
+critical = _logger.critical
+error = _logger.error
+warning = _logger.warning
+
+del _logger, _stream_handler, _formatter
